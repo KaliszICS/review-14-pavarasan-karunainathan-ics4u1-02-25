bd19fc2fb985476190f0e8940e62096ad52021b7
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-02-27 15:09:30 +0000

    changed method minStrings to minString

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -17,7 +17,7 @@ public class PracticeProblem{
 		return words.length <= MAXIMUM;
 	}
 
-	static String minStrings(String str1, String str2, String str3){
+	static String minString(String str1, String str2, String str3){
 		str1 = str1.toLowerCase();
 		str2 = str2.toLowerCase();
 		str3 = str3.toLowerCase();
